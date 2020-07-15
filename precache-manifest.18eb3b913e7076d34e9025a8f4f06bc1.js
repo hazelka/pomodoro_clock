cac2d940e16b9764b6f49c770083e2ddb2cce00e
Hazel Feng <hazel@Hazels-MacBook-Air.local>
2020-07-14 17:23:50 -0700

    Updates

---
+++ precache-manifest.18eb3b913e7076d34e9025a8f4f06bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f98e6428699bb9aaa562658ff14ab0b",
+    "url": "/pomodoro_clock/index.html"
+  },
+  {
+    "revision": "805ef03e16a809771b2a",
+    "url": "/pomodoro_clock/static/css/main.ff0d9e92.chunk.css"
+  },
+  {
+    "revision": "1a66ad753046c98ecdfb",
+    "url": "/pomodoro_clock/static/js/2.c2ca4a9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pomodoro_clock/static/js/2.c2ca4a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "805ef03e16a809771b2a",
+    "url": "/pomodoro_clock/static/js/main.9d814384.chunk.js"
+  },
+  {
+    "revision": "876221ed443e52accde0",
+    "url": "/pomodoro_clock/static/js/runtime-main.e6f5db23.js"
+  }
+]);
